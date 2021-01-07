df227d014e3517f7646d563e77d7f0c49e2a000c
CaioProgramming <caioalvesferreiracontato@gmail.com>
2021-01-06 23:42:35 -0300

    solved import bug on base model

--- app/src/main/java/com/creat/motiv/model/BaseModel.kt
+++ app/src/main/java/com/creat/motiv/model/BaseModel.kt
@@ -11,7 +11,12 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.firestore.*
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
