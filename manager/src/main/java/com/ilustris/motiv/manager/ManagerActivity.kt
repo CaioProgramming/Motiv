a401c414735df9005788ae32443b31c49802b065
CaioProgramming <caioalvesferreiracontato@gmail.com>
2022-01-20 13:30:41 -0300

    manager home ready

--- manager/src/main/java/com/ilustris/motiv/manager/ManagerActivity.kt
+++ manager/src/main/java/com/ilustris/motiv/manager/ManagerActivity.kt
@@ -1,6 +1,7 @@
 package com.ilustris.motiv.manager
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -23,50 +24,27 @@ class ManagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Giphy.configure(this, GIPHY_KEY)
-        //window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
         managerBinding = ActivityManagerBinding.inflate(layoutInflater)
         setContentView(managerBinding!!.root)
         managerBinding?.setupView()
     }
 
     private fun ActivityManagerBinding.setupView() {
-        setSupportActionBar(mainContent.motivTop.motivToolbar)
+        setSupportActionBar(mainContent.motivToolbar)
+        val toggle = ActionBarDrawerToggle(
+            this@ManagerActivity,
+            drawer,
+            mainContent.motivToolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
         val navController = findNavController(R.id.manager_nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(navController.graph, drawer)
         navView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    private fun NavigationView.setupNavigation() {
-        setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_home -> {
-                    changeFragment(ManagerHomeFragment())
-                    true
-                }
-                R.id.navigation_covers -> {
-                    changeFragment(CoversFragment())
-                    true
-                }
-                R.id.navigation_icons -> {
-                    changeFragment(IconsFragment())
-                    true
-                }
-                R.id.navigation_styles -> {
-                    changeFragment(StylesFragment())
-                    true
-                }
-                else -> true
-            }
-        }
-    }
-
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .setCustomAnimations(R.anim.slide_in_right, R.anim.fui_slide_out_left)
-            .replace(R.id.manager_nav_host_fragment, fragment)
-            .commit()
-    }
 
 }
